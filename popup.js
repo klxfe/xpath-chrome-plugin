22f698c8fed25641d304001270530a6fd660e1a2
klxfe <jaguar2374@gmail.com>
2023-03-27 18:18:48 +0200

    deleted comments + console.log

--- popup.js
+++ popup.js
@@ -1,13 +1,7 @@
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) =>{
     async function prepareTable(){
-        // let arrayName = ['One', 'Two', 'Three'];
-        // let arrayXpath = ['//div1', '//div2', '//div3'];
 
         let container = "<table><tr><th>Name</th><th>XPath</th></tr>";
-
-        // for(let i = 0; i < arrayName.length; i++){
-        //     container += `<tr><td>${arrayName[i]}</td><td>${arrayXpath[i]}</td></tr>`
-        // }
 
         message.forEach(function (element){
             container += `<tr><td>${element.name}</td><td>${element.xpath}</td></tr>`
@@ -15,8 +9,6 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) =>{
 
         document.getElementById('container').innerHTML = container;
     }
-    console.log(message);
-    console.log(sender);
     sendResponse('hi');
     prepareTable();
 })
