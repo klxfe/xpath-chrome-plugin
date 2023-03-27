cf059a9f5ae3600fb68a2941f01219752809e068
klxfe <jaguar2374@gmail.com>
2023-03-27 20:45:27 +0200

    finished functionallity

---
+++ content.js
@@ -0,0 +1,50 @@
+const update = function() {
+    setTimeout(() => {
+        const data = [];
+        const collection = document.getElementsByTagName('button')
+        for (let i = 0; i < collection.length; i++) {
+            let j = i + 1;
+            let obj = {
+                name: getName(collection.item(i)),
+                xpath: getXPathForElement(collection.item(i))
+            };
+            data.push(obj);
+        }
+        chrome.runtime.sendMessage(
+            data,
+            (response) => {
+                console.log('response');
+            })
+    }, 1000)
+}
+
+function getXPathForElement(element) {
+    const idx = (sib, name) => sib
+        ? idx(sib.previousElementSibling, name || sib.localName) + (sib.localName == name)
+        : 1;
+    const segs = elm => !elm || elm.nodeType !== 1
+        ? ['']
+        : elm.id && document.getElementById(elm.id) === elm
+            ? [`id("${elm.id}")`]
+            : [...segs(elm.parentNode), `${elm.localName.toLowerCase()}[${idx(elm)}]`];
+    return segs(element).join('/');
+}
+
+function getName(element) {
+    if (element.getAttribute('id')) {
+        return element.getAttribute('id');
+    }
+    if (element.getAttribute('class')) {
+        return element.getAttribute('class');
+    } else {
+        return 'button';
+    }
+}
+
+chrome.runtime.onMessage.addListener(
+    function(request, sender, sendResponse) {
+        update();
+        sendResponse('success');
+    });
+
+
--- popup.js
+++ popup.js
@@ -12,3 +12,11 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) =>{
     sendResponse('hi');
     prepareTable();
 })
+
+document.getElementById("refresh").onclick = function() {
+
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        chrome.tabs.sendMessage(tabs[0].id, 'update', function(response) {
+        });
+    });
+}
